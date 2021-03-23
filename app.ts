8d026449d0de849204f0a696f654368086cc858e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-23 21:43:41 +0100

    Add middleware

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import todoRoutes from './routes/todos';
 
 const app = express();
-
+app.use(todoRoutes);
+app.use(bodyParser.json());
 app.listen(3000);
-
