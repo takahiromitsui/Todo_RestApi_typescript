87298e9a9daf25b6357bfeb335ebfd61afb86157
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-24 10:08:38 +0100

    Add RequestBody type

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+
 let todos: Todo[] = [];
 const router = Router();
 
@@ -9,9 +11,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+	const body = req.body as RequestBody;
 	const newTodo: Todo = {
 		id: new Date().toISOString(),
-		text: req.body.text,
+		text: body.text,
 	};
 	todos.push(newTodo);
 	res.status(200).json({ message: 'Add todo', todo: newTodo, todos: todos });
@@ -20,11 +23,12 @@ router.post('/todo', (req, res, next) => {
 //Replace todo
 router.put('/todo/:todoId', (req, res, next) => {
 	const todoId = req.params.todoId;
+	const body = req.body as RequestBody;
 	const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
 	if (todoIndex >= 0) {
 		todos[todoIndex] = {
 			id: todos[todoIndex].id,
-			text: req.body.text,
+			text: body.text,
 		};
 		return res.status(200).json({ message: 'Updated todo', todos: todos });
 	}
