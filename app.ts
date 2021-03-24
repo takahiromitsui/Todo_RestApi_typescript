d50daceb008f515ffe88c3e5dd6d330d9fc7f0df
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-24 09:51:46 +0100

    Fix the issue of body.Parser by moving bodyParser

--- app.ts
+++ app.ts
@@ -4,6 +4,6 @@ import bodyParser from 'body-parser';
 import todoRoutes from './routes/todos';
 
 const app = express();
-app.use(todoRoutes);
 app.use(bodyParser.json());
+app.use(todoRoutes);
 app.listen(3000);
